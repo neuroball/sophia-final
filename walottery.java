4b560cc8a415e84cf8867842cd17583e7003fb90
Schnarchendorf, Oliver <neuroball@gmail.com>
2026-06-07 10:06:44 -0700

    [code] Create helper function.

--- walottery.java
+++ walottery.java
@@ -9,6 +9,8 @@ class walottery {
     int noOfGames;
     String inputType;
     int[][] gameNumbersArr;
+    int[][] drawnNumbersArr;
+    int[] numbersWonLookup = {0, 0, 3, 30, 1000, 1000000};
     int gameBound = 6;
     int lotteryBound = 49;
 
@@ -42,10 +44,10 @@ class walottery {
         }
     }
     
-    public void fillGamesWithQuickPick() {
+    public void fillArrayWithRandomNumbers(int[][] targetArr) {
         Random rand = new Random();
 
-        for (int currentGame = 0; currentGame < gameBound; currentGame++) {
+        for (int currentGame = 0; currentGame < noOfGames; currentGame++) {
             Set<Integer> gameNumbersSet = new HashSet<Integer>();
             while (true) {
                 int n = rand.nextInt(lotteryBound);
@@ -56,7 +58,7 @@ class walottery {
                     break;
                 }
             }
-            gameNumbersArr[currentGame] = gameNumbersSet.stream()
+            targetArr[currentGame] = gameNumbersSet.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
         }
@@ -66,15 +68,15 @@ class walottery {
         System.out.println("");
         
         Scanner scanner = new Scanner(System.in);
-        for (int currentGame = 0; currentGame < gameBound; currentGame++) {
+        for (int currentGame = 0; currentGame < noOfGames; currentGame++) {
             try {
                 System.out.println("");
-                System.out.println(String.format("Please enter your six comma separated lotto game numbers (1-49) for game %d.%n", currentGame + 1));
+                System.out.println(String.format("Please enter six comma separated lotto game numbers (1-49) for each game %d.%n", currentGame + 1));
 
                 String gameNumbers = scanner.nextLine();
                 String gameNumbersTrimmed = gameNumbers.replaceAll("\\s+", "");
 
-                boolean validContent = gameNumbersTrimmed.matches("[0-9],]+");
+                boolean validContent = gameNumbersTrimmed.matches("[[0-9],]+");
                 if (!validContent) {
                     throw new Exception("The input only can contain numbers [0-9] and commas.");
                 }
@@ -97,8 +99,13 @@ class walottery {
         }
     }
 
+    public void fillGamesWithQuickPick() {
+        fillArrayWithRandomNumbers(this.gameNumbersArr);
+    }
+
     public void createGames() {
         this.gameNumbersArr = new int[this.noOfGames][6];
+        this.drawnNumbersArr = new int[this.noOfGames][6];
 
         if (this.inputType.equalsIgnoreCase("q")) {
             fillGamesWithQuickPick();
@@ -107,39 +114,15 @@ class walottery {
         }   
     }
 
+    public void createDrawnNumbers() {
+        fillArrayWithRandomNumbers(this.drawnNumbersArr);
+    }
+
 }
 
 
-// If the variable 'inputType' contains 'm' in lower case, start a loop for each game.  For each game from 1-'noOfGames'
-//     Read list of comma separated game numbers from user input and store it in 'gameNumbers' variable.
-//     Clean input by removing spaces from 'gameNumbers' variable.
-//     Clean input by removing commas and store it in the 'gameNumbersTrimmed' variable.
-//     Check that the 'gameNumbersTrimmed' variable only contains numbers between 0-9 and nothing else.
-//       If something else is found, throw an error and display the error message to only use numbers when entering games.
-//     Break up the 'gameNumbersTrimmed' variable by splitting it by commas into an array named 'gameNumbersArr'..
-//     Verify length of 'gameNumbersArr' to be of length equaling 6.
-//       If the length is not 6, throw an error and display an error message to the user saying that each game has exactly 6 numbers.
-//     Assign 'gameNumbersArr' to the current game.
-//       Increment 'currentGame by 1
 
-// Create a drawnNumbers array of arrays of six integers.
-// Start a loop to create the drawn lotto numbers:
 
-//   For each game from 1-'noOfGames'
-//     Create an empty Set 'drawnNumbersSet'.
-//     Create a random number between 1 and 49 called 'draw'.
-//     Check 'drawnNumbersSet' for the number in 'draw'.
-//       If 'draw' doesn't exist in 'drawnNumbersSet'?
-//         Add it.
-//       Else
-//         If it already exists go back to draw a number… 
-//     Is the length of 'drawNumbersSet' 6?
-//       No
-//         Draw another number.
-//       Yes
-//         Convert it to an array called 'drawnNumbersArr'.
-//         Push 'drawnNumbersArr' to 'drawnNumbers.
-//    
 // Define double variable 'moneySpent'.
 // Define 'numbersWonLookup' as array of int and set it to [0, 0, 3, 30, 1000, 1000000]Define int variable 'moneyWon'.
 // Define int variable 'moneyWonThisGame
